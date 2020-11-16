1658e7d9e173f34313d2e5766f4f7feef67736e8
home <274534565@qq.com>
2020-11-16 22:21:37 +0800

    init compiler

---
+++ jdk.javadoc/jdk/javadoc/internal/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.javadoc.internal.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.4+10-LTS" },
+            { "jdk", "11.0.4" },
+            { "release", "11.0.4" },
+        };
+    }
+}
---
+++ jdk.jdeps/com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.4+10-LTS" },
+            { "jdk", "11.0.4" },
+            { "release", "11.0.4" },
+        };
+    }
+}
